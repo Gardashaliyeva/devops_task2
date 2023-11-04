e129537ff391898c7afabcbe56376207d67d5fe6
Ilaha Gardashaliyeva <ilahagardashaliyeva@gmail.com>
2023-11-04 15:27:01 +0400

    first commit

---
+++ test/app.test.js
@@ -0,0 +1,46 @@
+const request = require('supertest');
+const app = require('../app'); 
+
+describe('GET /', () => {
+  it('responds with Hello World', async () => {
+    const response = await request(app).get('/');
+    expect(response.statusCode).toBe(200);
+    expect(response.text).toBe('Hello World!');
+  });
+});
+
+describe('POST /echo', () => {
+  it('echoes the message', async () => {
+    const message = 'Hello testing';
+    const response = await request(app)
+      .post('/echo')
+      .send({ message });
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body.echo).toBe(message);
+  });
+});
+
+describe('GET /add/:num1/:num2', () => {
+  it('responds with the sum of two numbers', async () => {
+    const response = await request(app).get('/add/5/10');
+    expect(response.statusCode).toBe(200);
+    expect(response.body.sum).toBe(15);
+  });
+
+  it('handles invalid numbers', async () => {
+    const response = await request(app).get('/add/foo/bar');
+    expect(response.statusCode).toBe(400);
+    expect(response.text).toBe('Bad request');
+  });
+});
+
+afterAll((done) => {
+  const server = require('../server'); 
+  server.close(done);
+});
+
+
+
+
+
